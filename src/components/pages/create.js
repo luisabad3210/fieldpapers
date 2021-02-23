f8273b05fdf288e77488dc8ce1b7277898b18389
Luis <luisabad3210@gmail.com>
2021-02-23 13:01:32 -0500

    adding create wireframe

--- src/components/pages/create.js
+++ src/components/pages/create.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import SearchImg from '../images/Vector_search_icon.svg'
 
 const Create = () => {
     return (
-        <h1>
-            Create
-        </h1>
+        <div className='Create'>
+            <div className='welcomeContainer'>
+                <div className='welcomeLeft'>
+                    <h1>Create</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+                    <div className='searchInputContainer'>
+                        <input className='searchInput' placeholder='Search Area'></input>
+                        <button className='searchBtn'><img src={SearchImg} width='25' height='25'/></button>
+                    </div>
+                </div>
+
+                <div className='welcomeRight'>
+                {/* <img src={} alt='' title='' width='220px' height='220px'/> */}
+                img
+                </div>
+            </div>
+        </div>
     )
 }
 
