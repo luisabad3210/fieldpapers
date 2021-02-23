e52b25467862e7f12f66d2d5063cb3da2eb45ffa
Luis <luisabad3210@gmail.com>
2021-02-23 11:31:06 -0500

    adding wireframe to watch page

--- src/components/pages/watch.js
+++ src/components/pages/watch.js
@@ -2,10 +2,60 @@ import React from 'react'
 
 const Watch = () => {
     return (
-        <div>
-            <h1>
-                Watch
-            </h1>
+        <div className='watchContainer'>
+
+            <h1>Watch</h1>
+
+            <div className='watchMapSection'>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+                <div className='watchMapContainer'>
+                    <div className='watchMap'>map</div>
+                    <div>
+                        <div>untitled</div>
+                        <div>anonymous</div>
+                        <div>1 page , 1 hour ago</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
